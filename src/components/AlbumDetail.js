5ac0864087e3b6de76c11449988fc68740025019
Diego Collares <diego.collares@gmail.com>
2017-01-06 18:36:49 -0200

    reate text title Album

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {View, Text } from 'react-native';
+
+const AlbumDetail = (props) => {
+
+  return(
+
+    <View>
+      <Text>{props.album.title} </Text>
+    </View>
+
+  );
+
+};
+
+export default AlbumDetail;
